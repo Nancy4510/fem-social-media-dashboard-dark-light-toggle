75136f3aa610a03e489b6a8213c24936b19f18fe
Nancy4510 <nancy4510@gmail.com>
2024-12-18 16:53:05 -0500

    Minor changes to files

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ function watchTask() {
 }
 
 // Default Gulp Task
-exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+//exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
 
 // Build Gulp Task
 exports.build = series(scssTask, jsTask);
